f1ad2345368f7b5d1dbd37b6224a12eb8feb394e
julialiebs <jlieberman@scu.edu>
2020-05-17 13:25:23 -0600

    added photos. no code changes

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,4 +1,5 @@
 /*!
+Julia Lieberman CS169 project
     * Start Bootstrap - Creative v6.0.1 (https://startbootstrap.com/themes/creative)
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-creative/blob/master/LICENSE)
